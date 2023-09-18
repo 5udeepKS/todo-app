1c2830e70f92d8b67dd283cd8a11aa4170e027e6
5udeepKS <sudeepkanchibailu@gmail.com>
2023-09-19 02:07:32 +0530

    Added Model and removed dummy html data

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
+  serviceUrl: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.serviceUrl = "http://localhost:3000/tasks"
+  }
 }
